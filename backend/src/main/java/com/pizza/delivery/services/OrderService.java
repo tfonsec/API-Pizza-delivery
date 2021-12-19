2e5c794750522443d3a0faea615477ceb7dcae4f
Thiago Fonseca <thifonseca85@gmail.com>
2021-12-19 13:34:07 -0300

    POST /orders

--- backend/src/main/java/com/pizza/delivery/services/OrderService.java
+++ backend/src/main/java/com/pizza/delivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.pizza.delivery.services;
 
+import java.time.Instant;
 import java.util.List;
 
 import java.util.stream.Collectors;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pizza.delivery.dto.OrderDTO;
-
+import com.pizza.delivery.dto.ProductDTO;
 import com.pizza.delivery.entities.Order;
-
+import com.pizza.delivery.entities.OrderStatus;
+import com.pizza.delivery.entities.Product;
 import com.pizza.delivery.repositories.OrderRespository;
+import com.pizza.delivery.repositories.ProductRepository;
 
 
 
@@ -23,10 +26,29 @@ public class OrderService {
 	@Autowired
 	private OrderRespository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List <OrderDTO> findaAll() {
 		List <Order> list =  orderRepository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO orderDTO) {
+		Order order = new Order(null, orderDTO.getAddress(),
+				orderDTO.getLatitude(), 
+				orderDTO.getLongitude(),
+				Instant.now(),
+				OrderStatus.PENDING);
+		for(ProductDTO productDTO : orderDTO.getProducts()) {
+			Product product = productRepository.getById(productDTO.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
